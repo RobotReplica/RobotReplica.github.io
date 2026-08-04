45c159f306dd589f87a019655e1edbe44a193545
Yu Xiang <yu.xiang@utdallas.edu>
2026-08-04 18:03:50 -0500

    Use official Franka image for MIT site

--- app/page.tsx
+++ app/page.tsx
@@ -113,9 +113,9 @@ const sites = [
     mapPosition: { left: "91%", top: "22%" },
     robot: "Franka Panda",
     benchmark: "RobotReplica Franka Panda site",
-    image: "/franka-panda-single.png",
-    imageAlt: "Single Franka Panda seven-degree-of-freedom robot arm with gripper",
-    imageCredit: ["Image: EmPRISE Lab at Cornell", "https://emprise.cs.cornell.edu/people/"],
+    image: "/franka-research-3-official.jpg",
+    imageAlt: "Close-up of the single-arm Franka Research 3 robot",
+    imageCredit: ["Image: Franka Robotics", "https://franka.de/products-overview"],
     description:
       "A hosted Franka Panda evaluation site extending the network with a new reproducible manipulation benchmark.",
     partnerRole: "Developing a Franka Panda site and a new suite of standardized manipulation tasks.",
